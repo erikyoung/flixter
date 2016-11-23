d54fd24acd11c2eef1b6ced6a2a15c4d6aaff546
ErikYoung <erik.young@rmit.edu.vn>
2016-11-23 23:41:00 +0700

    trying to add flash messages

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,11 +12,9 @@ class LessonsController < ApplicationController
 
   
   def require_authorized_for_current_course
-   if  !current_user.enrolled_in?(current_lesson.section.course)  flash[:alert] = "Invalid login"
-    
+   if  !current_user.enrolled_in?(current_lesson.section.course),  flash[:alert] = "Invalid login"
       redirect_to course_path(current_lesson.section.course)
     end
-
   end
   helper_method :current_lesson
   def current_lesson
