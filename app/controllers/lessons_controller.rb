e654b766313345cdd1811ff335bbd2de6380a90a
ErikYoung <erik.young@rmit.edu.vn>
2017-01-02 11:44:35 +0700

    adjustment to lesson controller, allows for access to course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson, only: [:show]
+  before_action :require_authorized_for_current_course
   
 
   def show
@@ -9,16 +9,21 @@ class LessonsController < ApplicationController
 
   private
 
-  
-  def require_authorized_for_current_lesson
-    
-   if  !current_user.enrolled_in?(current_lesson.section.course)
-       redirect_to course_path(current_lesson.section.course), alert: 'Dude, seriously?'
-    end
-  end
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  
+  def require_authorized_for_current_course
+    
+   this_course = current_lesson.section.course
+    
+    if !current_user.enrolled_in?(this_course) && this_course.user != current_user
+      redirect_to course_path(this_course), alert: 'Dude, seriously?'
+    end
+  end
+  
+  
 end
 
