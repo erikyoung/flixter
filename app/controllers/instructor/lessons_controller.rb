11ee7b5c630b487b45bc385afc107026ccc9ca4e
ErikYoung <erik.young@rmit.edu.vn>
2017-01-04 03:19:40 +0700

    playing with rails console to get user_id

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,14 +1,19 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
   
 
+
+  def new
+    @lesson = Lesson.new
+  end
 
   def create
     @lesson = @current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@current_section.course)
   end 
+end
 
   
   def update
@@ -17,6 +22,7 @@ class Instructor::LessonsController < ApplicationController
   end
 
   private
+
 
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
@@ -28,27 +34,33 @@ class Instructor::LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  helper_method :current_section
-  def current_section
-    if params[:section_id].present?
-    @current_section ||= Section.find(params[:section_id])
-  else
-    current_lesson.section
-  end
-end
-
-
-  def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
-  end 
-
-
   def require_authorized_for_current_section
     if current_section.course.user != current_user
       return render text: 'Unauthorized', status: :unauthorized
     end
   end
+
+  helper_method :current_section
+  def current_section
+    if params[:section_id].present?
+      @current_section ||= Section.find(params[:section_id])  
+  end
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+  end 
 end
+
+
+  
+
+  
+
+
+  
+
+
+  
 
   
 
