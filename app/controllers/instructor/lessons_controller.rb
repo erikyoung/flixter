d7d1c86e421d2e0921e10aa5e23d08b2d30f6176
ErikYoung <erik.young@rmit.edu.vn>
2017-01-04 22:53:48 +0700

    fixed errors in js console

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,19 +3,13 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
   
-
-
-  def new
-    @lesson = Lesson.new
-  end
+  
 
   def create
     @lesson = @current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@current_section.course)
   end 
-end
 
-  
   def update
     current_lesson.update_attributes(lesson_params)
     render text: 'updated!'
@@ -34,22 +28,24 @@ end
     @current_lesson ||= Lesson.find(params[:id])
   end
 
-  def require_authorized_for_current_section
-    if current_section.course.user != current_user
-      return render text: 'Unauthorized', status: :unauthorized
-    end
-  end
 
   helper_method :current_section
   def current_section
-    if params[:section_id].present?
       @current_section ||= Section.find(params[:section_id])  
   end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
   end 
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render text: 'Unauthorized', status: :unauthorized
+    end
+  end
 end
+
+
 
 
   
