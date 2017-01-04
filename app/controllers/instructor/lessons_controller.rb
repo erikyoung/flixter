bbee12cc14f0cd1015dbaf3fdf2c2f6d78f6ebdd
ErikYoung <erik.young@rmit.edu.vn>
2017-01-05 01:29:35 +0700

    trying to update row_order

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -3,12 +3,15 @@ class Instructor::LessonsController < ApplicationController
   before_action :require_authorized_for_current_section, only: [:new, :create]
   before_action :require_authorized_for_current_lesson, only: [:update]
   
-  
+  def new
+    @lesson = Lesson.new
+  end
 
   def create
     @lesson = @current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(@current_section.course)
   end 
+
 
   def update
     current_lesson.update_attributes(lesson_params)
@@ -31,7 +34,10 @@ class Instructor::LessonsController < ApplicationController
 
   helper_method :current_section
   def current_section
-      @current_section ||= Section.find(params[:section_id])  
+    if params[:section_id].present?
+      @current_section ||= Section.find(params[:section_id]) 
+    else
+      current_lesson.section
   end
 
   def lesson_params
